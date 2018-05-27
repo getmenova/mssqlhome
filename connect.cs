2cdb5e2c9d7083fb06436ad885eec887f2119034
Michael A Nowiszewski <micski@gmail.com>
2018-05-27 21:42:07 +0000

    k

---
+++ connect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
